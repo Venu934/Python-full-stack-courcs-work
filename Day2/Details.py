4af1338d13a213fdd5d4589ad061c4e117a00772
VENU GOPAL KONDAVEETI <131335099+Venu934@users.noreply.github.com>
2026-03-10 17:08:47 +0530

    Add files via upload

---
+++ Day2/Details.py
@@ -0,0 +1,15 @@
+Student_Name = 'Kondaveety Venu Gopala Swamy'
+Student_ID = 'CGH2937'
+baltch_no = 'PFS-HYD-050'
+Email_ID = 'kondaveetivenugopal949@gmail.com'
+Date_of_Birth = '2004-02-02'
+Age = 22
+Gender = 'Male'
+Blood_Group = 'O+'
+City = 'Eluru'
+State = 'Andhra Pradesh'
+Student_Phone_Number = 9347957375
+Parent_Phone_Number = 8985956856
+Github_Link = 'https://github.com/Venu934'
+
+print(Student_Name,Student_ID,baltch_no,Email_ID,Date_of_Birth,Age,Gender,Blood_Group,City,State,Student_Phone_Number,Parent_Phone_Number,Github_Link, sep="\n")
